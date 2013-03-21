81fff525e2417a0e9e646cd9ba7846a589329139
Virpi <virpi.peuralinna@businesscollege.fi>
2013-03-21 11:15:32 +0200

    Lisäsin tulostuksen

--- Olioharjoituksia/src/Uusikokeilu.java
+++ Olioharjoituksia/src/Uusikokeilu.java
@@ -1,4 +1,7 @@
 
 public class Uusikokeilu {
-
+	public static void main(String[] args){
+		//t‰h‰n teen omat kokeiluni jotka ei haittaa maasteria
+		System.out.println("mites t‰‰ oikeen toimii.");
+	}
 }
